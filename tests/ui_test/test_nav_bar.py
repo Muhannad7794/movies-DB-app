b9268b9fd9761875bb3a2b2f4c74b3705e58b569
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2023-12-22 18:12:24 +0100

    added the first E2E test

---
+++ tests/ui_test/test_nav_bar.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+@pytest.fixture(scope="module")
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+def test_movies_navigation(driver):
+    driver.get("http://localhost:3307")
+    movies_link = driver.find_element(By.CSS_SELECTOR, "nav .nav-links a:first-child")
+    movies_link.click()
+    assert driver.find_element(By.CLASS_NAME, "movie-list")
+
+
+def test_directors_navigation(driver):
+    driver.get("http://localhost:3307")
+    directors_link = driver.find_element(
+        By.CSS_SELECTOR, "nav .nav-links a[href='/directors']"
+    )
+    directors_link.click()
+    assert driver.find_element(By.CLASS_NAME, "movie-list")
+
+
+def test_studios_navigation(driver):
+    driver.get("http://localhost:3307")
+    studios_link = driver.find_element(
+        By.CSS_SELECTOR, "nav .nav-links a[href='/studios']"
+    )
+    studios_link.click()
+    assert driver.find_element(By.CLASS_NAME, "movie-list")
