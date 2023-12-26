e2b3237f6c2e5128563b9980551565073db3cf19
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2023-12-26 14:23:44 +0100

    fixing the images preview in the movie-details window

--- frontend_movies_db/src/components/MovieDetails.js
+++ frontend_movies_db/src/components/MovieDetails.js
@@ -5,24 +5,6 @@ import './MovieDetails.css';
 function MovieDetails({ movie }) {
   const navigate = useNavigate();
 
-  React.useEffect(() => {
-    document.body.style.backgroundImage = `url(${movie.poster_url})`;
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundPosition = 'center';
-    document.body.style.margin = '0';
-    document.body.style.padding = '0';
-    // Set a class or style on body for other adjustments as needed
-
-    return () => {
-      // Revert body styles when the component unmounts
-      document.body.style.backgroundImage = '';
-      document.body.style.backgroundSize = '';
-      document.body.style.backgroundPosition = '';
-      document.body.style.margin = '';
-      document.body.style.padding = '';
-    };
-  }, [movie.poster_url]);
-
   const handleDirectorClick = () => {
     navigate(`/directors/${movie.director.id}`);
   };
@@ -31,20 +13,18 @@ function MovieDetails({ movie }) {
     navigate(`/studios/${movie.studio.id}`);
   };
 
-
-  // Add a style object for background image
   const detailsStyle = {
     backgroundImage: `url(${movie.poster_url})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
-    position: "relative",
+    borderRadius: '15px', // Rounded corners for the image
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)', // Shadow for depth
+    // Set height via aspect ratio here if needed
   };
 
-
   return (
-    <div className="movie-details" style={detailsStyle}>
-      <div className="movie-details-overlay"></div>
-      <div className="movie-details-content">
+    <div className="movie-details-container">
+      <div className="movie-details-text">
         <h2>{movie.title}</h2>
         <p>Genre: {movie.genre}</p>
         <p>Release Year: {movie.release_year}</p>
@@ -55,6 +35,9 @@ function MovieDetails({ movie }) {
         <p onClick={handleStudioClick} className="interactive-text">
           Production Studio: {movie.studio.name}
         </p>
+      </div>
+      <div className="movie-details-image" style={detailsStyle}>
+        <img src={movie.poster_url} alt={movie.title} />
       </div>
     </div>
   );
