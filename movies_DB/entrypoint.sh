086227449918921eb354e949e9b601ced07c2c20
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-06-22 15:15:44 +0200

    moved teh staticfiles collection step to Dcokerfile

--- movies_DB/entrypoint.sh
+++ movies_DB/entrypoint.sh
@@ -12,25 +12,6 @@ echo "AZURE_ACCOUNT_KEY: $AZURE_ACCOUNT_KEY"
 echo "AZURE_MEDIA_CONTAINER: $AZURE_MEDIA_CONTAINER"
 echo "AZURE_STATIC_CONTAINER: $AZURE_STATIC_CONTAINER"
 
-# Apply database migrations
-echo "Applying database migrations..."
-if [ "$DJANGO_ENVIRONMENT" = "production" ]; then
-    echo "Using production settings"
-    python manage.py migrate --settings=movies_DB.settings_prod
-else
-    echo "Using development settings"
-    python manage.py migrate
-fi
-
-# Collect static files
-echo "Collecting static files..."
-if [ "$DJANGO_ENVIRONMENT" = "production" ]; then
-    echo "Collecting static files using production settings"
-    python manage.py collectstatic --noinput --settings=movies_DB.settings_prod
-else
-    python manage.py collectstatic --noinput
-fi
-
-# Start server
+# start the server
 echo "Starting server..."
 exec "$@"
