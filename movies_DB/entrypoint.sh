9a5a7e1435c2ec3f21606c5bbafb1a180c183503
Muhannad Al Ulaby <mual0001@stud.kea.dk>
2024-06-23 15:35:42 +0200

    setting up the deployment configs with local testing for prod env

--- movies_DB/entrypoint.sh
+++ movies_DB/entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Source the .env file
-set -a
-source /movies-app/.env
-set +a
+# Source the .env file if it exists
+if [ -f /movies-app/.env ]; then
+    set -a
+    source /movies-app/.env
+    set +a
+fi
 
 # Debugging: Print environment variables to confirm they are loaded
-echo "SECRET_KEY: $SECRET_KEY"
-echo "AZURE_ACCOUNT_NAME: $AZURE_ACCOUNT_NAME"
-echo "AZURE_ACCOUNT_KEY: $AZURE_ACCOUNT_KEY"
-echo "AZURE_MEDIA_CONTAINER: $AZURE_MEDIA_CONTAINER"
-echo "AZURE_STATIC_CONTAINER: $AZURE_STATIC_CONTAINER"
+echo "Starting the application in $DJANGO_ENVIRONMENT mode..."
 
-# start the server
-echo "Starting server..."
-exec "$@"
+# Check for production environment and prepare the app
+if [ "$DJANGO_ENVIRONMENT" = "production" ]; then
+    echo "Running database migrations..."
+    python manage.py migrate --noinput
+    echo "Collecting static files..."
+    python manage.py collectstatic --noinput --clear
+    echo "Starting Gunicorn server on port 8000..."
+    exec gunicorn movies_DB.wsgi:application --bind 0.0.0.0:8000 --log-level debug --capture-output --timeout 120
+else
+    echo "Starting Django development server..."
+    exec python manage.py runserver 0.0.0.0:8000
+fi
